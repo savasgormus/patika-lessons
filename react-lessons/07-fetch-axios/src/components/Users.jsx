96329ef65af08f03adcddb0ad32c07a729e72664
Savaş Görmüş <savasgormus1@gmail.com>
2022-08-12 13:58:28 +0300

    update

---
+++ react-lessons/07-fetch-axios/src/components/Users.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+const Users = () => {
+
+    const[users, setUsers] = useState([]);
+    const[isLoading, setIsLoading] = useState(true)
+
+    useEffect( ()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res) => res.json())
+        .then(data => {
+            setUsers(data)
+            setIsLoading(false)
+        })
+        .catch((e) => console.log(e))
+    },[] )
+      
+ 
+
+
+  return (
+    <div>
+        <h1>Users</h1>
+        {
+            isLoading && <div>Loading...</div>
+        }
+        {
+            users.map(user => <div key={user.id}>{user.name}</div>)
+        }
+    </div>
+  )
+}
+
+export default Users
